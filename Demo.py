93a7ef170d03c830a3652856be23459430f7a640
Parikshit Dnyaneshwar Mahajan <137431626+Parikshit-26@users.noreply.github.com>
2025-01-31 19:09:19 +0530

    small changes in pandas for iloc and matrix-vector multi. by using 1D list for matrix in demo python file

--- Demo.py
+++ Demo.py
@@ -10,3 +10,35 @@ for i in range(3):
     print(i)
 
 print("Hello world")
+
+a = [[1,2,3], 
+     [4,5,6],
+     [7,8,9]]
+print(a)
+
+m = int(input("Enter the value of m: "))
+n = int(input("Enter the value of n: "))
+# create a list of size m*n
+A= []
+for i in range(0,m*n):
+    A.append(int(input("Enter the entry: ")))
+print("Now enter the entry of vector b")    
+b = []
+for j in range(0,n):
+    b.append(int(input("Enter the entry: ")))
+
+p = len(b)
+print(p)
+# print(len(A))
+# l = len(A)/p
+# print(l)
+Ab = []
+for k in range(0,m*n,p):
+    ele = 0
+    for i in range(0,p):
+        ele += (A[k+i]*b[i])
+    Ab.append(ele)
+
+
+print(Ab)
+
